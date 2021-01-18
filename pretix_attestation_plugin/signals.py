d99aee4f1c0be715812e05605b2fc8464fb24d09
Nurtas <inurtas1999@gmail.com>
2021-01-19 00:30:54 +0600

    Update email placeholder class

--- pretix_attestation_plugin/signals.py
+++ pretix_attestation_plugin/signals.py
@@ -7,6 +7,5 @@ from pretix.base.email import SimpleFunctionalMailTextPlaceholder
 
 @receiver(register_mail_placeholders, dispatch_uid="placeholder_custom")
 def register_mail_renderers(sender, **kwargs):
-    return SimpleFunctionalMailTextPlaceholder(
-        'code', ['order'], lambda order: order.code, sample='F8VVL'
-    )
+    from .email import OrderAttestationPlaceholder
+    return OrderAttestationPlaceholder()
