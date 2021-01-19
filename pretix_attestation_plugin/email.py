5ddc5d35224138abb8c83a72ae4f8772eacc9835
Nurtas Ilyas <nurtas.ilyas@nu.edu.kz>
2021-01-19 07:04:41 +0600

    Add plugin scaffold (#8)
    * add email placeholder plugin
    
    * Update setup.py
    
    Co-authored-by: ligi <ligi@ligi.de>
    
    * Update PluginApp pretix compatibility version
    
    * Update email placeholder class
    
    Co-authored-by: ligi <ligi@ligi.de>

---
+++ pretix_attestation_plugin/email.py
@@ -0,0 +1,20 @@
+from pretix.base.email import BaseMailTextPlaceholder
+
+class OrderAttestationPlaceholder(BaseMailTextPlaceholder):
+    def __init__(self):
+        self._identifier = "order_attestation_link"
+    
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def required_context(self):
+        return ['event']
+    
+    def render(self, context):
+        # Change to attestation link
+        return "This is Link"
+
+    def render_sample(self, event):
+        return "http://localhost/?ticket=MIGZMAoCAQYCAgTRA…"
---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+from distutils.command.build import build
+
+from setuptools import find_packages, setup
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        from django.core import management
+        management.call_command('compilemessages', verbosity=1)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
+setup(
+    name='pretix-attestation-placeholder-plugin',
+    version='0.1.0',
+    description='Pretix Ethereum Plugin Developers',
+    long_description=long_description,
+    url='https://github.com/efdevcon/pretix-attestation-placeholder-plugin',
+    author='Pretix Ethereum Plugin Developers',
+    author_email='ticketing@devcon.org',
+    license='MIT License',
+
+    install_requires=[
+        "pretix>=3.8.0",
+    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretix.plugin]
+pretix_attestation_plugin=pretix_attestation_plugin:PretixPluginMeta
+""",
+)
