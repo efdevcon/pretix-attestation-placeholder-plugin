a8fb5207d8aedbaa71c5483d69a14720e270b4f5
ligi <ligi@ligi.de>
2021-10-20 21:21:21 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ extras_require['dev'] = (
 
 setup(
     name='pretix-attestation-placeholder-plugin',
-    version='0.1.9',
+    version='0.2.0',
     description='Pretix Ethereum Plugin Developers',
     long_description=long_description,
     url='https://github.com/efdevcon/pretix-attestation-placeholder-plugin',
